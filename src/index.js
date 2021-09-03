2fc8b57177ed912f20bd5401d86f407aed0f02d4
Milan <mricoul@beapi.fr>
2021-09-03 14:28:28 +0200

    refactor (Accordion) : move priset into index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import Accordion from './classes/Accordion'
 import Modal from './classes/Modal'
 import Tabs from './classes/Tabs'
 import './css/style.css'
 
+Accordion.preset = {
+  '#accordion-demo-1': {},
+  '#accordion-demo-2': { hasAnimation: true },
+  '#accordion-demo-3': { allowMultiple: true },
+  '#accordion-demo-4': { forceExpand: false },
+}
+
+Accordion.initFromPreset()
 Modal.initFromPreset()
 Tabs.initFromPreset()
