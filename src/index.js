eed485eaa3fc9d2d0103c491e8cf553a217b25d7
Milan <mricoul@beapi.fr>
2021-09-03 14:30:19 +0200

    fix (index) : init Toggle

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Modal from './classes/Modal'
 import Tabs from './classes/Tabs'
+import Toggle from './classes/Toggle'
 import './css/style.css'
 
 Modal.initFromPreset()
 Tabs.initFromPreset()
+Toggle.initFromPreset()
