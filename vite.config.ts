f4be7b16a639d63321b48240865efecf93e7bb6a
mricoul <mricoul@beapi.fr>
2022-09-15 23:54:15 +0200

    build (vite): make vite build working

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
+
+const publicPath = 'examples'
 
 export default defineConfig({
   optimizeDeps: {
@@ -7,6 +10,16 @@ export default defineConfig({
   build: {
     commonjsOptions: {
       include: [/body-scroll-lock/, /node_modules/],
+    },
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        accordion: resolve(__dirname, `${publicPath}/accessible-accordion/index.html`),
+        dropdown: resolve(__dirname, `${publicPath}/accessible-dropdown/index.html`),
+        modal: resolve(__dirname, `${publicPath}/accessible-modal/index.html`),
+        tabs: resolve(__dirname, `${publicPath}/accessible-tabs/index.html`),
+        toggle: resolve(__dirname, `${publicPath}/accessible-toggle/index.html`),
+      },
     },
   },
 })
