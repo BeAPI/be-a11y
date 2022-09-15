d2f988c7e44933ea7b9e7f938b6accfff4e9e264
mricoul <mricoul@beapi.fr>
2022-09-15 23:17:50 +0200

    feat (js): add entry file exports

---
+++ src/be-a11y.js
@@ -0,0 +1,7 @@
+import Accordion from './classes/Accordion.js'
+import Dropdown from './classes/Dropdown.js'
+import Modal from './classes/Modal.js'
+import Tabs from './classes/Tabs.js'
+import Toggle from './classes/Toggle.js'
+
+export { Accordion, Dropdown, Modal, Tabs, Toggle }
