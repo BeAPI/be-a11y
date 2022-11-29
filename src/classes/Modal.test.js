70ab3c4fbdb8ae4a986973fd2da132343b44cd23
mricoul <mricoul@beapi.fr>
2022-11-29 18:46:44 +0100

    fix (tests): replace jest with playwright

--- src/classes/Modal.test.js
+++ src/classes/Modal.test.js
@@ -1,14 +1,11 @@
-import puppeteer from 'puppeteer'
+import { expect, test } from '@playwright/test'
 
-let browser
-const app = `file://${process.cwd()}/examples/accessible-modal/index.html`
+test.describe('Modal', () => {
+  test.beforeEach(async({page}) => {
+    await page.goto('http://127.0.0.1:5173/examples/accessible-modal/index.html')
+  })
 
-describe('Modal', () => {
-  test('Click on a dialog trigger button, expect the modal dialog is visible.', async () => {
-    browser = await puppeteer.launch()
-    const page = await browser.newPage()
-  
-    await page.goto(app)
+  test('Click on a dialog trigger button, expect the modal dialog is visible.', async ({page}) => {
     await page.click('button[aria-controls="dialog-1"]')
     let display = await page.$eval(
       '#dialog-1',
@@ -16,28 +13,16 @@ describe('Modal', () => {
     )
 
     expect(display).toBe('block')
-
-    await page.close()
   })
   
-  test('Click on a dialog trigger button, expect the first focusable element in dialog is focused.', async () => {
-    browser = await puppeteer.launch()
-    const page = await browser.newPage()
-  
-    await page.goto(app)
+  test('Click on a dialog trigger button, expect the first focusable element in dialog is focused.', async ({page}) => {
     await page.click('button[aria-controls="dialog-1"]')
     const firstFocusableElement = await page.$(`#street`)
 
     expect(await page.evaluate(elem => window.document.activeElement === elem, firstFocusableElement)).toEqual(true)
-
-    await page.close()
   })
 
-  test('Click on the dialog close button, expect the modal dialog is hidden.', async () => {
-    browser = await puppeteer.launch()
-    const page = await browser.newPage()
-  
-    await page.goto(app)
+  test('Click on the dialog close button, expect the modal dialog is hidden.', async ({page}) => {
     await page.click('button[aria-controls="dialog-1"]')
     await page.click('#dialog-1-close')
     let display = await page.$eval(
@@ -46,15 +31,9 @@ describe('Modal', () => {
     )
 
     expect(display).toBe('none')
-
-    await page.close()
   })
 
-  test('Press "Escape" key when the modal dialog is visible, expect the modal dialog is hidden.', async () => {
-    browser = await puppeteer.launch()
-    const page = await browser.newPage()
-  
-    await page.goto(app)
+  test('Press "Escape" key when the modal dialog is visible, expect the modal dialog is hidden.', async ({page}) => {
     await page.click('button[aria-controls="dialog-1"]')
     await page.keyboard.press('Escape')
     let display = await page.$eval(
@@ -63,15 +42,9 @@ describe('Modal', () => {
     )
 
     expect(display).toBe('none')
-
-    await page.close()
   })
 
-  test('Open the modal dialog, expect the last focusable element in modal dialog is focused after pressing "Shift Tab" key.', async () => {
-    browser = await puppeteer.launch()
-    const page = await browser.newPage()
-  
-    await page.goto(app)
+  test('Open the modal dialog, expect the last focusable element in modal dialog is focused after pressing "Shift Tab" key.', async ({page}) => {
     await page.click('button[aria-controls="dialog-1"]')
     await page.keyboard.down('ShiftLeft')
     await page.keyboard.press('Tab')
@@ -80,7 +53,5 @@ describe('Modal', () => {
     const closeButton = await page.$(`#dialog-1-close`)
 
     expect(await page.evaluate(elem => window.document.activeElement === elem, closeButton)).toEqual(true)
-
-    await page.close()
   })
 })
--- src/classes/Toggle.test.js
+++ src/classes/Toggle.test.js
@@ -1,14 +1,11 @@
-import puppeteer from 'puppeteer'
+import { expect, test } from '@playwright/test'
 
-let browser
-const app = `file://${process.cwd()}/examples/accessible-toggle/index.html`
-
-describe('Toggle', () => {
-  test('Click on a toggle button, expect the content is visible.', async () => {
-    browser = await puppeteer.launch()
-    const page = await browser.newPage()
+test.describe('Toggle', () => {
+  test.beforeEach(async({page}) => {
+    await page.goto('http://127.0.0.1:5173/examples/accessible-toggle/index.html')
+  })
   
-    await page.goto(app)
+  test('Click on a toggle button, expect the content is visible.', async ({page}) => {
     await page.click('button[aria-controls="toggle-1"]')
     let display = await page.$eval(
       '#toggle-1',
@@ -16,18 +13,13 @@ describe('Toggle', () => {
     )
 
     expect(display).toBe('block')
-
-    await page.close()
   })
   
-  test('On a 1025px window width page, click on a toggle button of a content who is visible only for <= 1024px window width pages, expect the content is still hidden.', async () => {
-    browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.setViewport({
+  test('On a 1025px window width page, click on a toggle button of a content who is visible only for <= 1024px window width pages, expect the content is still hidden.', async ({page}) => {
+    await page.setViewportSize({
       width: 1025,
       height: 100,
     })
-    await page.goto(app)
     await page.click('button[aria-controls="toggle-4"]')
     let display = await page.$eval(
       '#toggle-4',
@@ -35,18 +27,13 @@ describe('Toggle', () => {
     )
 
     expect(display).toBe('none')
-
-    await page.close()
   })
 
-  test('On a 1024px window width page, click on a toggle button of a content who is visible only for <= 1024px window width pages, expect the content is visible.', async () => {
-    browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.setViewport({
+  test('On a 1024px window width page, click on a toggle button of a content who is visible only for <= 1024px window width pages, expect the content is visible.', async ({page}) => {
+    await page.setViewportSize({
       width: 1024,
       height: 100,
     })
-    await page.goto(app)
     await page.click('button[aria-controls="toggle-4"]')
     let display = await page.$eval(
       '#toggle-4',
@@ -54,15 +41,9 @@ describe('Toggle', () => {
     )
 
     expect(display).toBe('block')
-
-    await page.close()
   })
 
-  test('Click on a toggle button of a visible content, expect the content is hidde5.', async () => {
-    browser = await puppeteer.launch()
-    const page = await browser.newPage()
-  
-    await page.goto(app)
+  test('Click on a toggle button of a visible content, expect the content is hidde5.', async ({page}) => {
     await page.click('button[aria-controls="toggle-5"]')
     let display = await page.$eval(
       '#toggle-5',
@@ -70,7 +51,5 @@ describe('Toggle', () => {
     )
 
     expect(display).toBe('none')
-
-    await page.close()
   })
 })
