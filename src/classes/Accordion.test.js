d3628dfead1115d69c111b330381bb0a272ed445
mricoul <mricoul@beapi.fr>
2023-01-02 15:21:40 +0100

    fix (Accordion): prevent dupplicate ids

--- src/classes/Accordion.test.js
+++ src/classes/Accordion.test.js
@@ -6,14 +6,16 @@ test.describe('Accordion', () => {
   })
 
   test('Click the first tab accordion, expect the first focusable element in the panel is focused.', async ({page}) => {
-    await page.click('#accordion-1')
+    const id = await page.locator('#accordion-demo-1').getAttribute('data-id')
+    await page.click(`#accordion-${id}-1`)
     await expect(page.locator('#cufc1-1')).toBeFocused()
   })
 
   test('Click the second tab accordion, expect the second panel is visible.', async ({page}) => {
-    await page.click('#accordion-2')
+    const id = await page.locator('#accordion-demo-1').getAttribute('data-id')
+    await page.click(`#accordion-${id}-2`)
     const display = await page.$eval(
-      '#accordion-panel-2',
+      `#accordion-${id}-panel-2`,
       dialog => window.getComputedStyle(dialog).display
     )
 
@@ -21,49 +23,58 @@ test.describe('Accordion', () => {
   })
 
   test('Press "ArrowDown" key when the first tab accordion is focused, expect the second tab accordion is focused.', async ({page}) => {
-    await page.focus('#accordion-1')
+    const id = await page.locator('#accordion-demo-1').getAttribute('data-id')
+    await page.focus(`#accordion-${id}-1`)
     await page.keyboard.press('ArrowDown')
-    const secondAccordionTab = await page.$(`#accordion-2`)
+    const secondAccordionTab = await page.$(`#accordion-${id}-2`)
 
     expect(await page.evaluate(elem => window.document.activeElement === elem, secondAccordionTab)).toEqual(true)
   })
 
   test('Press "ArrowUp" key when the first tab accordion is focused, expect the last tab accordion is focused.', async ({page}) => {
-    await page.focus('#accordion-1')
+    const id = await page.locator('#accordion-demo-1').getAttribute('data-id')
+    await page.focus(`#accordion-${id}-1`)
     await page.keyboard.press('ArrowUp')
-    const lastAccordionTab = await page.$(`#accordion-3`)
+    const lastAccordionTab = await page.$(`#accordion-${id}-3`)
 
     expect(await page.evaluate(elem => window.document.activeElement === elem, lastAccordionTab)).toEqual(true)
   })
 
   test('Press "ArrowUp" key when the last tab accordion is focused, expect the second tab accordion is focused.', async ({page}) => {
-    await page.focus('#accordion-3')
+    const id = await page.locator('#accordion-demo-1').getAttribute('data-id')
+    await page.focus(`#accordion-${id}-3`)
     await page.keyboard.press('ArrowUp')
-    const secondAccordionTab = await page.$(`#accordion-2`)
+    const secondAccordionTab = await page.$(`#accordion-${id}-2`)
 
     expect(await page.evaluate(elem => window.document.activeElement === elem, secondAccordionTab)).toEqual(true)
   })
 
   test('Press "ArrowDown" key when the last tab accordion is focused, expect the first tab accordion is focused.', async ({page}) => {
-    await page.focus('#accordion-3')
+    const id = await page.locator('#accordion-demo-1').getAttribute('data-id')
+
+    await page.focus(`#accordion-${id}-3`)
     await page.keyboard.press('ArrowDown')
-    const firstAccordionTab = await page.$(`#accordion-1`)
+    const firstAccordionTab = await page.$(`#accordion-${id}-1`)
 
     expect(await page.evaluate(elem => window.document.activeElement === elem, firstAccordionTab)).toEqual(true)
   })
 
   test('Press "Home" key when the last tab accordion is focused, expect the first tab accordion is focused.', async ({page}) => {
-    await page.focus('#accordion-3')
+    const id = await page.locator('#accordion-demo-1').getAttribute('data-id')
+
+    await page.focus(`#accordion-${id}-3`)
     await page.keyboard.press('Home')
-    const firstAccordionTab = await page.$(`#accordion-1`)
+    const firstAccordionTab = await page.$(`#accordion-${id}-1`)
 
     expect(await page.evaluate(elem => window.document.activeElement === elem, firstAccordionTab)).toEqual(true)
   })
 
   test('Press "End" key when the first tab accordion is focused, expect the last tab accordion is focused.', async ({page}) => {
-    await page.focus('#accordion-1')
+    const id = await page.locator('#accordion-demo-1').getAttribute('data-id')
+
+    await page.focus(`#accordion-${id}-1`)
     await page.keyboard.press('End')
-    const lastAccordionTab = await page.$(`#accordion-3`)
+    const lastAccordionTab = await page.$(`#accordion-${id}-3`)
 
     expect(await page.evaluate(elem => window.document.activeElement === elem, lastAccordionTab)).toEqual(true)
   })
