c42d190d0c55f630deba499a7d5557b7c8760d06
mricoul <mricoul@beapi.fr>
2023-11-20 15:46:16 +0100

    refactor (classes): move handle function as private function

--- src/classes/Tabs.js
+++ src/classes/Tabs.js
@@ -14,71 +14,80 @@ class Tabs extends AbstractDomElement {
     }
 
     this.focus = false
-    this.handleButtonBlur = this.handleButtonBlur.bind(this)
-    this.handleButtonClick = this.handleButtonClick.bind(this)
-    this.handleButtonFocus = this.handleButtonFocus.bind(this)
-    this.handleKeydown = this.handleKeydown.bind(this)
-    this.focusPreviousTab = this.focusPreviousTab.bind(this)
-    this.focusNextTab = this.focusNextTab.bind(this)
+    this._handleButtonBlur = handleButtonBlur.bind(this)
+    this._handleButtonClick = handleButtonClick.bind(this)
+    this._handleButtonFocus = handleButtonFocus.bind(this)
+    this._handleKeydown = handleKeydown.bind(this)
+    this._focusPreviousTab = focusPreviousTab.bind(this)
+    this._focusNextTab = focusNextTab.bind(this)
+    this._focusFirstTab = focusFirstTab.bind(this)
+    this._focusLastTab = focusLastTab.bind(this)
     this.close = this.close.bind(this)
     this.init()
   }
 
   /**
    * Initialization
+   *
    * @returns {void}
+   *
    * @author Milan Ricoul
    */
   init() {
-    this.applyToButtons((button) => button.addEventListener('click', this.handleButtonClick))
-    this.applyToButtons((button) => button.addEventListener('focus', this.handleButtonFocus))
-    this.applyToButtons((button) => button.addEventListener('blur', this.handleButtonBlur))
-    document.addEventListener('keydown', this.handleKeydown)
+    const buttons = this._element.querySelectorAll(this._settings.tabListSelector)
+
+    buttons.forEach((button) => {
+      button.addEventListener('click', this._handleButtonClick)
+      button.addEventListener('focus', this._handleButtonFocus)
+      button.addEventListener('blur', this._handleButtonBlur)
+    })
+
+    document.addEventListener('keydown', this._handleKeydown)
   }
 
   /**
    * Destroy method
+   *
    * @returns {void}
+   *
    * @author Milan Ricoul
    */
   destroy() {
-    this.applyToButtons((button) => button.removeEventListener('click', this.handleButtonClick))
+    const buttons = this._element.querySelectorAll(this._settings.tabListSelector)
+
+    buttons.forEach((button) => button.removeEventListener('click', this.handleButtonClick))
+
     document.removeEventListener('keydown', this.handleKeydown)
   }
 
   /**
    * Unselect all tab buttons
+   *
    * @returns {void}
+   *
    * @author Milan Ricoul
    */
   unselectAllButtons() {
-    this.applyToButtons((button) => button.setAttribute('aria-selected', 'false'))
-  }
+    const buttons = this._element.querySelectorAll(this._settings.tabListSelector)
 
-  /**
-   * Execute a function for every tab buttons
-   * @returns {void}
-   * @param {Function} func callback
-   * @author Milan Ricoul
-   */
-  applyToButtons(func) {
-    const el = this._element
-    const s = this._settings
-    const buttons = el.querySelectorAll(s.tabListSelector)
-
-    Array.prototype.slice.call(buttons).forEach((button) => func(button))
+    buttons.forEach((button) => button.setAttribute('aria-selected', 'false'))
   }
 
   /**
    * Open tab panel
+   *
    * @param {HTMLElement} button clicked button
+   *
    * @returns {void}
+   *
    * @author Milan Ricoul
    */
   open(button) {
-    this.applyToButtons((btn) => this.close(btn))
-
+    const buttons = this._element.querySelectorAll(this._settings.tabListSelector)
     const panel = document.getElementById(button.getAttribute('aria-controls'))
+
+    buttons.forEach((button) => this.close(button))
+
     button.focus()
     button.setAttribute('aria-selected', 'true')
     button.removeAttribute('tabindex')
@@ -87,8 +96,11 @@ class Tabs extends AbstractDomElement {
 
   /**
    * Open tab panel
+   *
    * @param {HTMLElement} button clicked button
+   *
    * @returns {void}
+   *
    * @author Milan Ricoul
    */
   close(button) {
@@ -99,62 +111,10 @@ class Tabs extends AbstractDomElement {
   }
 
   /**
-   * Focus the previous tab. If not previous tag, focus the last tab.
-   * @returns {void}
-   * @author Milan Ricoul
-   */
-  focusPreviousTab() {
-    const activeElement = document.activeElement
-
-    if (activeElement && activeElement.parentNode.getAttribute('role') === 'tablist') {
-      const previousButton = activeElement.previousElementSibling || activeElement.parentNode.lastElementChild
-
-      this._settings.auto ? this.open(previousButton) : previousButton.focus()
-    }
-  }
-
-  /**
-   * Focus the next tab. If not next tab, focus the first tab.
-   * @returns {void}
-   * @author Milan Ricoul
-   */
-  focusNextTab() {
-    const activeElement = document.activeElement
-
-    if (activeElement && activeElement.parentNode.getAttribute('role') === 'tablist') {
-      const nextButton = activeElement.nextElementSibling || activeElement.parentNode.firstElementChild
-
-      this._settings.auto ? this.open(nextButton) : nextButton.focus()
-    }
-  }
-
-  /**
-   * Focus the first tab.
-   * @returns {void}
-   * @author Milan Ricoul
-   */
-  focusFirstTab() {
-    const firstTab = this._element.querySelectorAll(this._settings.tabListSelector)[0]
-
-    this._settings.auto ? this.open(firstTab) : firstTab.focus()
-  }
-
-  /**
-   * Focus the last tab.
-   * @returns {void}
-   * @author Milan Ricoul
-   */
-  focusLastTab() {
-    const tabs = this._element.querySelectorAll(this._settings.tabListSelector)
-    const tabsCount = tabs.length
-    const lastTab = tabs[tabsCount - 1]
-
-    this._settings.auto ? this.open(lastTab) : lastTab.focus()
-  }
-
-  /**
    * Remove the focused tab, then focus the first tab.
+   *
    * @returns {void}
+   *
    * @author Milan Ricoul
    */
   removeTab() {
@@ -172,67 +132,139 @@ class Tabs extends AbstractDomElement {
       this.open(firstTab)
     }
   }
+}
 
-  /**
-   * Handle tab button click
-   * @param {MouseEvent} e Mouse click event
-   * @returns {void}
-   * @author Milan Ricoul
-   */
-  handleButtonClick(e) {
-    const clickedButton = e.currentTarget
-    const isSelected = clickedButton.getAttribute('aria-selected') === 'true'
+/**
+ * Handle tab button click
+ *
+ * @param {MouseEvent} e Mouse click event
+ *
+ * @returns {void}
+ *
+ * @author Milan Ricoul
+ */
+function handleButtonClick(e) {
+  const clickedButton = e.currentTarget
+  const isSelected = clickedButton.getAttribute('aria-selected') === 'true'
 
-    if (!isSelected) {
-      this.open(clickedButton)
-    }
+  if (!isSelected) {
+    this.open(clickedButton)
+  }
+}
+
+/**
+ * Focus the previous tab. If not previous tag, focus the last tab.
+ *
+ * @returns {void}
+ *
+ * @author Milan Ricoul
+ */
+function focusPreviousTab() {
+  const activeElement = document.activeElement
+
+  if (activeElement && activeElement.parentNode.getAttribute('role') === 'tablist') {
+    const previousButton = activeElement.previousElementSibling || activeElement.parentNode.lastElementChild
+
+    this._settings.auto ? this.open(previousButton) : previousButton.focus()
+  }
+}
+
+/**
+ * Focus the next tab. If not next tab, focus the first tab.
+ *
+ * @returns {void}
+ *
+ * @author Milan Ricoul
+ */
+function focusNextTab() {
+  const activeElement = document.activeElement
+
+  if (activeElement && activeElement.parentNode.getAttribute('role') === 'tablist') {
+    const nextButton = activeElement.nextElementSibling || activeElement.parentNode.firstElementChild
+
+    this._settings.auto ? this.open(nextButton) : nextButton.focus()
+  }
+}
+
+/**
+ * Focus the first tab.
+ *
+ * @returns {void}
+ *
+ * @author Milan Ricoul
+ */
+function focusFirstTab() {
+  const firstTab = this._element.querySelectorAll(this._settings.tabListSelector)[0]
+
+  this._settings.auto ? this.open(firstTab) : firstTab.focus()
+}
+
+/**
+ * Focus the last tab.
+ *
+ * @returns {void}
+ *
+ * @author Milan Ricoul
+ */
+function focusLastTab() {
+  const tabs = this._element.querySelectorAll(this._settings.tabListSelector)
+  const tabsCount = tabs.length
+  const lastTab = tabs[tabsCount - 1]
+
+  this._settings.auto ? this.open(lastTab) : lastTab.focus()
+}
+
+/**
+ * Handle tab button focus
+ *
+ * @returns {void}
+ *
+ * @author Milan Ricoul
+ */
+function handleButtonFocus() {
+  this.focus = true
+}
+
+/**
+ * Handle tab button blur
+ *
+ * @returns {void}
+ *
+ * @author Milan Ricoul
+ */
+function handleButtonBlur() {
+  this.focus = false
+}
+
+/**
+ * Handle keyboard keydown
+ *
+ * @param {KeyboardEvent} e Keyboard keydown event
+ *
+ * @returns {void}
+ *
+ * @author Milan Ricoul
+ */
+function handleKeydown(e) {
+  if (!this.focus) {
+    return
   }
 
-  /**
-   * Handle tab button focus
-   * @returns {void}
-   * @author Milan Ricoul
-   */
-  handleButtonFocus() {
-    this.focus = true
-  }
-
-  /**
-   * Handle tab button blur
-   * @returns {void}
-   * @author Milan Ricoul
-   */
-  handleButtonBlur() {
-    this.focus = false
-  }
-
-  /**
-   * Handle keyboard keydown
-   * @param {KeyboardEvent} e Keyboard keydown event
-   * @returns {void}
-   * @author Milan Ricoul
-   */
-  handleKeydown(e) {
-    if (!this.focus) {
-      return
-    }
-
-    switch (e.code) {
-      case 'ArrowLeft':
-        this.focusPreviousTab()
-        break
-      case 'ArrowRight':
-        this.focusNextTab()
-        break
-      case 'Home':
-        this.focusFirstTab()
-        break
-      case 'End':
-        this.focusLastTab()
-        break
-      case 'Delete':
-        this.removeTab()
-    }
+  switch (e.code) {
+    case 'ArrowLeft':
+      this._focusPreviousTab()
+      break
+    case 'ArrowRight':
+      this._focusNextTab()
+      break
+    case 'Home':
+      this._focusFirstTab()
+      break
+    case 'End':
+      this._focusLastTab()
+      break
+    case 'Delete':
+      this.removeTab()
   }
 }
 
